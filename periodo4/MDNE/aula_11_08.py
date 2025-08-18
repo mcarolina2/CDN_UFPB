879a32a2309316f6ea8c79bf0113290a6146d1b0
Carol <108403441+mcarolina2@users.noreply.github.com>
2025-08-18 17:23:49 -0300

    organização das pastas.

--- periodo4/MDNE/aula_11_08.py
+++ periodo4/MDNE/aula_11_08.py
@@ -1,10 +1,12 @@
 import nltk
 import numpy as np 
 from nltk.corpus import gutenberg
+from nltk.corpus import europarl_raw
+
 
 # carregando o texto: 
-nltk.download('gutenberg')
-alice = gutenberg.raw(fileids='carroll-alice.txt')
+#nltk.download('gutenberg')
+#alice = gutenberg.raw(fileids='carroll-alice.txt')
 #print (alice) 
 
     #verificando o tamanho do texto
@@ -14,15 +16,15 @@ alice = gutenberg.raw(fileids='carroll-alice.txt')
 #print (alice[0:100])
 
     #Acessando o texto com numpy
-nltk.download('punkt_tab')
-default_st = nltk.sent_tokenize
-alice_senteces = default_st(text=alice)
+#nltk.download('punkt_tab')
+#default_st = nltk.sent_tokenize
+#alice_senteces = default_st(text=alice)
     #Testando 
 #print('\nTotal de sentenças dealice:', len(alice_senteces))
 #print('Cinco primeiras sentenças de alice:-', np.array(alice_senteces[0:5]))
 
     #Testanto outro idioma:
 nltk.download('europarl_raw')
-german_text = europarl_raw.german.raw(filesds='ep-00-01-17.de')
+german_text = europarl_raw.german.raw(fileids='ep-00-01-17.de')
 #total de caracteres do corpo:
 print(len(german_text))
